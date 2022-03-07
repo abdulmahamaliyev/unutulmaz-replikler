81940aae658b13656ec5b4aaf7ee242be3eec9e2
amaqa <amaqa@DESKTOP-S55S7U1>
2022-03-08 01:07:00 +0900

    added title for quote

--- replikler.js
+++ replikler.js
@@ -2,50 +2,66 @@
 
 module.exports = [
   {
+    replik: "Azdan az çoktan çok gider",
+    author: "Süleyman Çakır",
+    title: "Kurtlar Vadisi",
+  },
+  {
     replik: "Bitch!",
     author: "Jesse Pinkman",
+    title: "Breaking Bad",
   },
   {
     replik: "Speak into the mic, bitch.",
     author: "Jesse Pinkman",
+    title: "Breaking Bad",
   },
   {
     replik: "Clearly, his taste in women is the same as his taste in lawyers.",
     author: "Saul Goodman",
+    title: "Breaking Bad",
   },
   {
     replik: "Everyone sounds like Meryl Streep with a gun to their head.",
     author: "Mike Ehrmantraut",
+    title: "Breaking Bad",
   },
   {
     replik:
       "Just because you shot Jesse James doesn't mean you are Jesse James.",
     author: "Mike Ehrmantraut",
+    title: "Breaking Bad",
   },
   {
     replik: "I will put you under the jail.",
     author: "Hank Schrader",
+    title: "Breaking Bad",
   },
   {
     replik: "My name is ASAC Schrader, and you can go f*ck yourself.",
     author: "Hank Schrader",
+    title: "Breaking Bad",
   },
   {
     replik:
       "Put me on your magical boat, man, and sail me down your chocolaty river of meth!",
     author: "Badger",
+    title: "Breaking Bad",
   },
   {
     replik: "The Universe is Random. Not Inevitable. It's simple Chaos.",
     author: "Walter White",
+    title: "Breaking Bad",
   },
   {
     replik:
       "Sometimes it just feels better not to talk. At All. About Anything. To Anyone.",
     author: "Walter White",
+    title: "Breaking Bad",
   },
   {
     replik: "So roll me further bitch!",
     author: "Jesse Pinkman",
+    title: "Breaking Bad",
   },
 ];
