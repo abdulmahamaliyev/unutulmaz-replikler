599e1531389361992ba728476d8ac363fc286ca6
amaqa <amaqa@DESKTOP-S55S7U1>
2022-03-08 00:56:01 +0900

    fix names

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var express = require("express");
-var repliklerRepository = require("./repliklerRepository");
+var repliklerRepository = require("./replikTabani");
 var app = express();
 var port = process.env.PORT || 3001;
 
